576f64036e61d243ce6ddae594627eb9aa667cbe
Erickdez <erickhf16@outlook.com>
2021-02-20 21:55:43 -0600

    Fix Bugs

--- static/script.js
+++ static/script.js
@@ -1,19 +1,3 @@
-/* sum section*/
-
-function sum() {
-    var n1 = document.getElementById("number1").value
-    var n2 = document.getElementById("number2").value
-
-    var number1 = parseInt(n1)
-    var number2 = parseInt(n2)
-    var result = number1 + number2
-
-    var resultElement = document.getElementById("result")
-    resultElement.innerText = "sum result: " + result
-}
-
-/* sum section*/
-
 
 function hideDivById(divId) {
     hideAllDivW3Includes()
@@ -24,10 +8,21 @@ function hideDivById(divId) {
 }
 
 function hideAllDivW3Includes() {
-    var elementArray = document.getElementsByName("w3includes")
+    var elementArray = document.getElementsByName("pages")
     for (var element of elementArray) {
         element.style.display = "none"
     }
 }
 
 w3.includeHTML()
+
+//Send Contact email
+function sendEMail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    alert("De: " + name + " <" + email + "> \nMensaje:\n" + message);
+
+}
+
